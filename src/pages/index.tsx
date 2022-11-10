8aadc83fcb296b48719df91991031d4b8d8aecfd
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-09 22:12:14 -0300

    feat: create structure based in DDD

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import styles from '@/pages/index.module.css';
+import styles from '~/pages/index.module.css';
 
 export default function Home() {
-  var test = 1;
-  console.log(test);
   return (
     <div className={styles.container}>
       <Head>
