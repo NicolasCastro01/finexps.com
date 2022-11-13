0c15b09c8179a0a12281ec8f00eb05c0ba9101e0
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-13 01:08:09 -0300

    refactor: adjust lint

--- src/app/presentation/common/components/input-text/input-text.spec.tsx
+++ src/app/presentation/common/components/input-text/input-text.spec.tsx
@@ -3,38 +3,38 @@ import InputTextComponent from './input-text';
 import type { InputTextProps } from './interfaces';
 
 const defaultProps = {
-	placeholder: 'Text',
-	onChange: () => jest.fn()
+  placeholder: 'Text',
+  onChange: () => jest.fn()
 } as InputTextProps;
 
 const makeSut = () => render(<InputTextComponent {...defaultProps} />);
 
 describe('InputTextComponent', () => {
-	test('should render', () => {
-		makeSut();
-		const component = screen.getByRole('textbox', { name: 'Text' });
-		expect(component).toBeInTheDocument();
-	});
+  test('should render', () => {
+    makeSut();
+    const component = screen.getByRole('textbox', { name: 'Text' });
+    expect(component).toBeInTheDocument();
+  });
 
-	test('should render with placeholder', () => {
-		defaultProps.placeholder = 'Text 2';
-		makeSut();
-		const component = screen.getByRole('textbox', { name: 'Text 2' });
-		expect(component).toBeInTheDocument();
-	});
+  test('should render with placeholder', () => {
+    defaultProps.placeholder = 'Text 2';
+    makeSut();
+    const component = screen.getByRole('textbox', { name: 'Text 2' });
+    expect(component).toBeInTheDocument();
+  });
 
-	test('should render with error', () => {
-		defaultProps.error = true;
-		makeSut();
-		const component = screen.getByRole('textbox', { name: 'Text 2' });
-		expect(component).toHaveClass('ring-1 ring-red');
-	});
+  test('should render with error', () => {
+    defaultProps.error = true;
+    makeSut();
+    const component = screen.getByRole('textbox', { name: 'Text 2' });
+    expect(component).toHaveClass('ring-1 ring-red');
+  });
 
-	test('should render on change', () => {
-		defaultProps.onChange = jest.fn();
-		makeSut();
-		const component = screen.getByRole('textbox', { name: 'Text 2' });
-		fireEvent.change(component, { target: { value: 'typing' } });
-		expect(defaultProps.onChange).toHaveBeenCalled();
-	});
+  test('should render on change', () => {
+    defaultProps.onChange = jest.fn();
+    makeSut();
+    const component = screen.getByRole('textbox', { name: 'Text 2' });
+    fireEvent.change(component, { target: { value: 'typing' } });
+    expect(defaultProps.onChange).toHaveBeenCalled();
+  });
 });
