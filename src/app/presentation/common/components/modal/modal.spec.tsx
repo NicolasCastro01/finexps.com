873c14a6d3587c4be2e0741070a8ce516e9b74b4
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-16 23:58:07 -0300

    refactor: style of modal in breakpoint mobile

--- src/app/presentation/common/components/modal/modal.spec.tsx
+++ src/app/presentation/common/components/modal/modal.spec.tsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
 import ModalComponent from './modal';
 
-const makeSut = () => render(<ModalComponent />);
+const makeSut = () =>
+  render(
+    <ModalComponent isOpen={true} title='Modal Title'>
+      <div />
+    </ModalComponent>
+  );
 
 describe('ModalComponent', () => {
   test('should render', () => {
     makeSut();
+    // const component = screen.getByRole('dialog', { name: 'Modal Title' });
+    // expect(component).toBeInTheDocument();
   });
 });
--- src/app/presentation/common/components/modal/modal.tsx
+++ src/app/presentation/common/components/modal/modal.tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 import { CloseIcon } from '~/app/presentation/common/icons';
-
-interface ModalProps {
-  children: React.ReactNode;
-  title: string;
-  isOpen: boolean;
-}
+import type { ModalProps } from './interfaces';
 
 function ModalComponent({ children, title }: ModalProps) {
   const [close, setClose] = useState(false);
@@ -15,13 +10,13 @@ function ModalComponent({ children, title }: ModalProps) {
   };
 
   const Header = () => (
-    <div className='flex justify-start items-center px-48px'>
+    <div className='flex justify-start items-center px-48px sm:px-24px'>
       <h1 className='text-24px text-gray7 font-bold'>{title}</h1>
     </div>
   );
 
   const Body = () => (
-    <div className='relative flex flex-col justify-start items-center px-48px py-32px gap-24px'>
+    <div className='relative flex flex-col justify-start items-center px-48px py-32px gap-24px sm:px-24px'>
       {children}
     </div>
   );
@@ -32,11 +27,15 @@ function ModalComponent({ children, title }: ModalProps) {
     }
 
     return (
-      <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-1 outline-none focus:outline-none h-screen w-screen bg-gray-900 bg-opacity-50'>
-        <div className='bg-gray2 rounded-6px w-535px'>
+      <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-1 outline-none focus:outline-none h-screen w-screen bg-gray-900 bg-opacity-50 sm:items-end'>
+        <div
+          className='bg-gray2 rounded-6px w-535px sm:w-full sm:rounded-t-20px'
+          aria-modal='true'
+          role='dialog'
+        >
           <div className='flex justify-end pt-24px pr-24px'>
             <button
-              className='text-gray6 text-24px [&>svg]:fill-gray5'
+              className='text-gray6 text-24px [&>svg]:fill-gray5 sm:text-20px'
               aria-label='Fechar modal'
               onClick={handleClose}
             >
