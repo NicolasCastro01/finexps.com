7a16cbbe9c3bb0d531a0132af298081492579a9b
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-20 18:33:15 -0300

    feat: create function for tranform value to currency

---
+++ src/app/infra/utils/turn-into-currency.util.ts
@@ -0,0 +1,21 @@
+interface TurnIntoCurrencyProps {
+	value: number;
+	currencyDisplay?: 'symbol' | 'code' | 'name' | 'narrowSymbol';
+	style?: 'decimal' | 'currency';
+	locale?: 'pt-BR' | 'en-US';
+	currency?: 'BRL' | 'USD';
+}
+
+export const turnIntoCurrency = ({
+	value,
+	currencyDisplay = 'symbol',
+	style = 'currency',
+	locale = 'pt-BR',
+	currency = 'BRL'
+}: TurnIntoCurrencyProps) => {
+	return value.toLocaleString(locale, {
+		style,
+		currency,
+		currencyDisplay
+	});
+};
