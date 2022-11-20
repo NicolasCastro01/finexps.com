3040d44ba605661daa2a3fcb59e60aa5f3c4553a
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-20 18:30:33 -0300

    fix: imports of app in tests

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './'
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './'
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-
-    '^@/pages/(.*)$': '<rootDir>/pages/$1'
-  },
-  testEnvironment: 'jest-environment-jsdom'
+	roots: ['<rootDir>/src'],
+	testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	moduleNameMapper: {
+		'~/(.*)': '<rootDir>/src/$1',
+		'\\.(css|sass|scss)$': 'identity-obj-proxy'
+	}
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
