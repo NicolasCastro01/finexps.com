7b0085f978080ccae6c9a9f0f300d928507d6bbc
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-19 23:17:55 -0300

    feat: add component card in iCndex to test and alter background of layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import {
-  ButtonTag,
-  CloseIcon,
-  InputTextTag,
-  ModalTag
+  CardTag
 } from '~/app/presentation/common';
 import { GlobalLayoutTag } from '~/app/presentation/common/layouts';
 
@@ -12,22 +9,7 @@ export default function Page() {
   return (
     <GlobalLayoutTag title='Dev'>
       <h1 className='text-32px text-center text-gray6'>In development</h1>
-      <ModalTag title='Nova transação' isOpen={true} iconClose={<CloseIcon />}>
-        <InputTextTag
-          onChange={() => {
-            return;
-          }}
-          placeholder='Título'
-        />
-        <ButtonTag
-          backgroundTransparent={false}
-          onClick={() => {
-            return;
-          }}
-          variant='large'
-          label='Salvar'
-        />
-      </ModalTag>
+      <CardTag />
     </GlobalLayoutTag>
   );
 }
