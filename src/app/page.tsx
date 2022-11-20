d1c12cdef40d541179aeb44fc7acb41c330ff264
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-20 18:35:59 -0300

    refactor: add components in page for tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { CardValueTag } from '~/app/presentation/common';
+import { CardValueTag, ModalTag } from '~/app/presentation/common';
+import ArrowUpCircle from '~/app/presentation/common/icons/arrow-up-circle/arrow-up-circle';
 import { GlobalLayoutTag } from '~/app/presentation/common/layouts';
 
 export default function Page() {
   return (
     <GlobalLayoutTag title='Dev'>
       <h1 className='text-32px text-center text-gray6'>In development</h1>
-      <CardValueTag />
+      <CardValueTag
+        backgroundColor='gray'
+        icon={<ArrowUpCircle />}
+        title='Entradas'
+        info='Últimas 24 horas'
+        value={1000.8}
+        iconColor='red'
+      />
+
+      <ModalTag isOpen={true} title='Modal'>
+        <p>Modal content</p>
+      </ModalTag>
     </GlobalLayoutTag>
   );
 }
