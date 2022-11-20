021b1cf1ab3edf314246bc72abb1bac33a0fd926
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-20 18:34:02 -0300

    test: create test of card value component

--- src/app/presentation/common/components/card-value/card-value.spec.tsx
+++ src/app/presentation/common/components/card-value/card-value.spec.tsx
@@ -1,10 +1,35 @@
-import { render } from '@testing-library/react';
-import CardComponent from './card-value';
+import { render, screen } from '@testing-library/react';
+import CardValueComponent from './card-value';
+import type { CardValuesProps } from './interfaces';
 
-const makeSut = () => render(<CardComponent />);
+const defaultProps = {
+  title: 'Entradas',
+  info: 'Últimas 24 horas',
+  icon: <div />,
+  iconColor: 'green',
+  value: 1000.8,
+  backgroundColor: 'gray'
+} as CardValuesProps;
+
+const makeSut = () => render(<CardValueComponent {...defaultProps} />);
 
 describe('CardComponent', () => {
   test('should render', () => {
     makeSut();
+    const component = screen.getByTestId('card-value');
+    expect(component).toBeInTheDocument();
+  });
+
+  test('should render with background gray', () => {
+    makeSut();
+    const component = screen.getByTestId('card-value');
+    expect(component).toHaveClass('bg-gray4');
+  });
+
+  test('should render with background green', () => {
+    defaultProps.backgroundColor = 'green';
+    makeSut();
+    const component = screen.getByTestId('card-value');
+    expect(component).toHaveClass('bg-greenDark');
   });
 });
