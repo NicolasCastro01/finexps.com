2fd01c2abf43f94f1a4f0dc0df4b5157a8fcf303
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-11-30 00:02:34 -0300

    feat: add search icon in project

--- src/app/presentation/common/icons/index.ts
+++ src/app/presentation/common/icons/index.ts
@@ -2,3 +2,4 @@ export * from './arrow-down-circle';
 export * from './arrow-up-circle';
 export * from './close';
 export * from './dollar-circle';
+export * from './search';
---
+++ src/app/presentation/common/icons/search/search.tsx
@@ -0,0 +1,24 @@
+function Search() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='20'
+			height='20'
+			fill='none'
+			viewBox='0 0 20 20'
+		>
+			<path
+				fillRule='evenodd'
+				d='M8.969 2.781a6.188 6.188 0 100 12.375 6.188 6.188 0 000-12.375zM.719 8.97a8.25 8.25 0 1116.5 0 8.25 8.25 0 01-16.5 0z'
+				clipRule='evenodd'
+			></path>
+			<path
+				fillRule='evenodd'
+				d='M13.344 13.344a1.031 1.031 0 011.459 0l4.176 4.177a1.031 1.031 0 11-1.458 1.458l-4.177-4.176a1.031 1.031 0 010-1.459z'
+				clipRule='evenodd'
+			></path>
+		</svg>
+	);
+}
+
+export default Search;
