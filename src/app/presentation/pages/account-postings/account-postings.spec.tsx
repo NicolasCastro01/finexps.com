5101065f4a1ab0e8e5d20347e5ba4238cf0bc1fc
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-03 00:48:00 -0300

    test: create test for account postings pages

--- src/app/presentation/pages/account-postings/account-postings.spec.tsx
+++ src/app/presentation/pages/account-postings/account-postings.spec.tsx
@@ -1,10 +1,58 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import AccountPostingsPage from './account-postings';
 
 const makeSut = () => render(<AccountPostingsPage />);
 
-describe('AccountPostingsPage', () => {
-	test('should render', () => {
+describe('Account Postings Page', () => {
+	it('should render header', () => {
 		makeSut();
+		const header = screen.getByTestId('header');
+		expect(header).toBeInTheDocument();
+	});
+
+	it('should render main', () => {
+		makeSut();
+		const main = screen.getByTestId('main');
+		expect(main).toBeInTheDocument();
+	});
+
+	describe('Interaction resolution desktop', () => {
+		it('should click on new transaction button', () => {
+			makeSut();
+			const button = screen.getByRole('button', {
+				name: 'Botão Nova transação'
+			});
+			fireEvent.click(button);
+			expect(button).toBeInTheDocument();
+		});
+
+		it('should click on search button', () => {
+			makeSut();
+			const button = screen.getByRole('button', {
+				name: 'Botão Buscar'
+			});
+			fireEvent.click(button);
+			expect(button).toBeInTheDocument();
+		});
+
+		it('should click on search input', () => {
+			makeSut();
+			const input = screen.getByRole('textbox', {
+				name: 'Busque uma transação'
+			});
+			fireEvent.change(input, { target: { value: 'teste' } });
+			expect(input).toBeInTheDocument();
+		});
+	});
+
+	describe('Interaction resolution mobile', () => {
+		it('should click on new transaction button', () => {
+			makeSut();
+			const button = screen.getByRole('button', {
+				name: 'Botão Buscar Pequeno'
+			});
+			fireEvent.click(button);
+			expect(button).toBeInTheDocument();
+		});
 	});
 });
