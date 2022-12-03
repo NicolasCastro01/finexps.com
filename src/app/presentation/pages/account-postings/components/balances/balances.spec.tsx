58486e60616930bd711911c44cbbae232ac979bf
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-02 23:49:29 -0300

    test: create test for balance component

--- src/app/presentation/pages/account-postings/components/balances/balances.spec.tsx
+++ src/app/presentation/pages/account-postings/components/balances/balances.spec.tsx
@@ -1,10 +1,32 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import BalancesComponent from './balances';
 
 const makeSut = () => render(<BalancesComponent />);
 
-describe('BalancesComponent', () => {
-	test('should render', () => {
+describe('Balances component', () => {
+	it('should render', () => {
 		makeSut();
+		const component = screen.getByTestId('balances');
+		expect(component).toBeInTheDocument();
+	});
+
+	describe('Interaction', () => {
+		it('should move to down', () => {
+			makeSut();
+			const component = screen.getByTestId('balances');
+			fireEvent.mouseDown(component);
+			fireEvent.mouseMove(component, { movementX: 100 });
+			fireEvent.mouseUp(component);
+			expect(component).toBeInTheDocument();
+		});
+
+		it('should move to up', () => {
+			makeSut();
+			const component = screen.getByTestId('balances');
+			fireEvent.mouseDown(component);
+			fireEvent.mouseMove(component, { movementX: -100 });
+			fireEvent.mouseUp(component);
+			expect(component).toBeInTheDocument();
+		});
 	});
 });
