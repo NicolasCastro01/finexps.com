9ef4cedde4e3fd0faecac010a40ee74bfca45aa3
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-02 23:34:28 -0300

    test: create test for transaction component

--- src/app/presentation/pages/account-postings/components/transaction/transaction.spec.tsx
+++ src/app/presentation/pages/account-postings/components/transaction/transaction.spec.tsx
@@ -1,10 +1,37 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import type { TransactionProps } from './interfaces';
 import TransactionComponent from './transaction';
 
-const makeSut = () => render(<TransactionComponent />);
+const defaultProps = {
+	amount: 1000,
+	category: 'Venda',
+	date: '13/04/2022',
+	description: 'Descrição da transação',
+	type: 'outbound'
+} as TransactionProps;
 
-describe('TransactionComponent', () => {
-	test('should render', () => {
+const makeSut = () => render(<TransactionComponent {...defaultProps} />);
+
+describe('Transaction component', () => {
+	it('should render', () => {
 		makeSut();
+		const component = screen.getByTestId('transaction');
+		expect(component).toBeInTheDocument();
+	});
+
+	// test type
+	describe('Transaction type', () => {
+		it('should render outbound type', () => {
+			makeSut();
+			const labelAmount = screen.getByTestId('amount');
+			expect(labelAmount).toHaveClass('text-red');
+		});
+
+		it('should render inbound type', () => {
+			defaultProps.type = 'inbound';
+			makeSut();
+			const labelAmount = screen.getByTestId('amount');
+			expect(labelAmount).toHaveClass('text-greenLight');
+		});
 	});
 });
