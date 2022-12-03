c4f0b633dc49eb3995f898eb45684099e2887065
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-03 00:47:32 -0300

    refactor: alter account postings page for test unit

--- src/app/presentation/pages/account-postings/account-postings.tsx
+++ src/app/presentation/pages/account-postings/account-postings.tsx
@@ -5,13 +5,17 @@ import { BalancesTag, TransactionTag } from './components';
 function AccountPostingsPage() {
 	return (
 		<>
-			<header className='bg-gray1 h-212px py-40px px-160px md:px-20px sm:px-20px'>
+			<header
+				className='bg-gray1 h-212px py-40px px-160px md:px-20px sm:px-20px'
+				data-testid='header'
+			>
 				<div className='flex justify-end'>
 					<ButtonTag
 						backgroundTransparent={false}
 						onClick={() => console.log('click')}
 						variant='medium'
 						label='Nova transação'
+						ariaLabel='Botão Nova transação'
 					/>
 				</div>
 				<div className='mt-10'>
@@ -32,6 +36,7 @@ function AccountPostingsPage() {
 						variant='medium'
 						label='Buscar'
 						icon={<SearchIcon />}
+						ariaLabel='Botão Buscar'
 					/>
 				</div>
 				<div className='hidden sm:block'>
@@ -40,10 +45,14 @@ function AccountPostingsPage() {
 						onClick={() => console.log('click')}
 						variant='small'
 						icon={<SearchIcon />}
+						ariaLabel='Botão Buscar Pequeno'
 					/>
 				</div>
 			</section>
-			<main className='mt-7 px-160px md:px-20px sm:px-20px flex flex-col justify-between gap-8px'>
+			<main
+				className='mt-7 px-160px md:px-20px sm:px-20px flex flex-col justify-between gap-8px'
+				data-testid='main'
+			>
 				<TransactionTag
 					amount={1000}
 					category='Venda'
