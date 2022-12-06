f3e82ea515d78d5cbbbda311729e0e7960f3ab48
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-05 23:50:05 -0300

    feat: add arrow left and arrow right icons

---
+++ src/app/presentation/common/icons/arrow-right/arrow-right.tsx
@@ -0,0 +1,19 @@
+function ArrowRight() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='14'
+			height='24'
+			fill='none'
+			viewBox='0 0 14 24'
+		>
+			<path
+				fillRule='evenodd'
+				d='M.94.94a1.5 1.5 0 012.12 0l10 10a1.5 1.5 0 010 2.12l-10 10a1.5 1.5 0 01-2.12-2.12L9.878 12 .939 3.06a1.5 1.5 0 010-2.12z'
+				clipRule='evenodd'
+			></path>
+		</svg>
+	);
+}
+
+export default ArrowRight;
--- src/app/presentation/common/icons/index.ts
+++ src/app/presentation/common/icons/index.ts
@@ -1,4 +1,6 @@
 export * from './arrow-down-circle';
+export * from './arrow-left';
+export * from './arrow-right';
 export * from './arrow-up-circle';
 export * from './close';
 export * from './dollar-circle';
