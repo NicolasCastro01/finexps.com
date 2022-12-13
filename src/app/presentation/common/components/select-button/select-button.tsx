733edf5153fbf385a7b35ad89c2a3a7916af5590
viniciusanchieta <vinicius.anchieta@outlook.com>
2022-12-13 02:24:03 -0300

    refactor: add prop value, add aria-pressed and and separate test in scenarios of select button component

--- src/app/presentation/common/components/select-button/select-button.tsx
+++ src/app/presentation/common/components/select-button/select-button.tsx
@@ -5,7 +5,8 @@ function SelectButtonComponent({
 	label,
 	onClick,
 	selected,
-	variant
+	variant,
+	value
 }: SelectButtonProps) {
 	const handleVariant = () => {
 		if (selected) {
@@ -17,19 +18,21 @@ function SelectButtonComponent({
 		}
 
 		return {
-			error: 'bg-gray3 text-gray6 [&>svg]:fill-greenLight hover:bg-gray4',
+			error: 'bg-gray3 text-gray6 [&>svg]:fill-red hover:bg-gray4',
 			success: 'bg-gray3 text-gray6 [&>svg]:fill-greenLight hover:bg-gray4'
 		}[variant];
 	};
 	return (
 		<button
 			type='button'
-			className={`${handleVariant()} py-16px flex justify-center items-center max-w-211px min-w-159px rounded-6px text-16px cursor-pointer gap-8px duration-150 hover:duration-150`}
+			className={`${handleVariant()} py-16px flex justify-center items-center w-211px sm:w-159px rounded-6px text-16px cursor-pointer gap-8px duration-150 hover:duration-150`}
 			onClick={onClick}
 			aria-label={`Botão ${label}`}
+			value={value}
+			aria-pressed={selected}
 		>
 			{icon}
-			Entrada
+			{label}
 		</button>
 	);
 }
